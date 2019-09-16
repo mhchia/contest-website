5c2a8fdb27f4036cfa86073b056013cbbe0ebf64
mhchia <kevin.mh.chia@gmail.com>
2019-09-16 23:03:34 +0800

    Add api app
    - Models
    - Basic settings

---
+++ contest_website/api/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Registrant
+
+
+admin.site.register(Registrant)
---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+classifiers = [f"Programming Language :: Python :: {version}" for version in ["3.7"]]
+
+
+setuptools.setup(
+    name="contest-website",
+    version="0.0.1",
+    license="MIT",
+    platforms=["unix", "linux", "osx"],
+    classifiers=classifiers,
+    install_requires=[
+        "django>=2.2.5,<3.0.0",
+        "web3>=5.0.2,<6.0.0",
+    ],
+    packages=setuptools.find_packages(exclude=["tests", "tests.*"]),
+    zip_safe=False,
+)
